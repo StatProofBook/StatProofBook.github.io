e60436f378170940e2fb108c02647ca7d9977d0c
Joram Soch <joram.soch@bccn-berlin.de>
2019-09-27 15:13:39 +0200

    improved index generator
    The automatic index generator now also checks the (manually curated) table of contents for completeness and provides command window output.

--- indexes.py
+++ indexes.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 """
-Create Indices for The Book of Statistical Proofs
+Index Generator for The Book of Statistical Proofs
+_
+This script loads all files from the proof directory and
+- checks if they are in "Table of Contents" (structured overview)
+- writes them as a list into "Proof by Number" (chronological list)
+- writes them as a list into "Proof by Topic" (an alphabetical list)
+
+Author: Joram Soch, BCCN Berlin
+E-Mail: joram.soch@bccn-berlin.de
+
+First edit: 2019-09-27 12:55:00
+ Last edit: 2019-09-27 15:15:00
 """
 
 
@@ -8,6 +19,7 @@ Create Indices for The Book of Statistical Proofs
 #-----------------------------------------------------------------------------#
 import os
 import re
+import numpy as np
 from datetime import datetime
 
 # List files in proof directory
@@ -45,39 +57,71 @@ for file in files:
         
         # Write dictionary entry
         #---------------------------------------------------------------------#
-        proofs[proof_id] = [proof_id, shortcut, title, username, date]
+        proofs[proof_id] = {'proof_id': proof_id, 'shortcut': shortcut, 'title': title, \
+                            'username': username, 'date': date}
         pr_ids.append(proof_id)
         pr_nos.append(int(proof_id[1:]))
         titles.append(title)
-        
+
+# Output number of proof files
+#-----------------------------------------------------------------------------#
+print('\n-> TBSP Index Generator:')
+print('   - ' + str(len(proofs)) + ' files found in proof directory!')
+
+# Table of Contents: read index file
+#-----------------------------------------------------------------------------#
+print('\n1. "Table_of_Contents.md":')
+ind1 = open('Indexes/Table_of_Contents.md', 'r')
+tocs = ind1.readlines()
+ind1.close()
+
+# Table of Contets: check for Shortcuts
+#-----------------------------------------------------------------------------#
+incl = np.zeros(len(proofs), dtype=bool)
+for (i, proof) in enumerate(proofs):
+    for line in tocs:
+        if line.find('(/Proofs/' + proofs[proof]['shortcut'] + '.html)') > -1:
+            incl[i] = True
+    if ~incl[i]:
+        print('   - WARNING: proof "' + proofs[proof]['shortcut'] + '" is not in table of contents!')
+if all(incl):
+    print('   - ' + str(sum(incl)) + ' proofs found in table of contents!')
+
 # Proof by Number: prepare index file
 #-----------------------------------------------------------------------------#
+print('\n2. "Proof_by_Number.md":')
 ind2 = open('Indexes/Proof_by_Number.md', 'w')
 ind2.write('---\nlayout: page\ntitle: "Proof by Number"\n---\n\n\n')
-ind2.write('| ID | Shortcut | Theorem | Author | Date |\n')
-ind2.write('|:-- |:-------- |:------- |:------ |:---- |\n')
+ind2.write('| ID | Name | Theorem | Author | Date |\n')
+ind2.write('|:-- |:---- |:------- |:------ |:---- |\n')
 
 # Proof by Number: sort by Proof ID
 #-----------------------------------------------------------------------------#
 sort_ind = [i for (v, i) in sorted([(v, i) for (i, v) in enumerate(pr_nos)])]
 for i in sort_ind:
-    ind2.write('| ' + proofs[pr_ids[i]][0] + ' | ' + proofs[pr_ids[i]][1] + ' | [' + \
-               proofs[pr_ids[i]][2] + '](/Proofs/' + proofs[pr_ids[i]][1] + '.html) | ' + \
-               proofs[pr_ids[i]][3] + ' | ' + date.strftime('%Y-%m-%d') + ' |\n')
+    ind2.write('| ' + proofs[pr_ids[i]]['proof_id'] + ' | ' + proofs[pr_ids[i]]['shortcut'] + ' | [' + \
+               proofs[pr_ids[i]]['title'] + '](/Proofs/' + proofs[pr_ids[i]]['shortcut'] + '.html) | ' + \
+               proofs[pr_ids[i]]['username'] + ' | ' + proofs[pr_ids[i]]['date'].strftime('%Y-%m-%d') + ' |\n')
 ind2.close()
+print('   - successfully written to disk!')
 
 # Proof by Topic: prepare index file
 #-----------------------------------------------------------------------------#
+print('\n3. "Proof_by_Topic.md":')
 ind3 = open('Indexes/Proof_by_Topic.md', 'w')
 ind3.write('---\nlayout: page\ntitle: "Proof by Topic"\n---\n\n\n')
 
-# Proof by Topic: sort by Proof ID
+# Proof by Topic: sort by Title
 #-----------------------------------------------------------------------------#
 sort_ind = [i for (v, i) in sorted([(v, i) for (i, v) in enumerate(titles)])]
-ind3.write('### ' + proofs[pr_ids[sort_ind[0]]][2][0] + '\n\n')
-ind3.write('- [' + proofs[pr_ids[sort_ind[0]]][2] + '](/Proofs/' + proofs[pr_ids[sort_ind[0]]][1] + '.html)\n')
-for j in range(1,len(titles)):
-    if proofs[pr_ids[sort_ind[j]]][2][0] != proofs[pr_ids[sort_ind[j-1]]][2][0]:
-        ind3.write('\n### ' + proofs[pr_ids[sort_ind[j]]][2][0] + '\n\n')
-    ind3.write('- [' + proofs[pr_ids[sort_ind[j]]][2] + '](/Proofs/' + proofs[pr_ids[sort_ind[j]]][1] + '.html)\n')
+for i in range(0,len(titles)):
+    shortcut = proofs[pr_ids[sort_ind[i]]]['shortcut']
+    title    = proofs[pr_ids[sort_ind[i]]]['title']
+    if i == 0:
+        ind3.write('### ' + title[0] + '\n\n')
+    else:
+        if title[0] != proofs[pr_ids[sort_ind[i-1]]]['title'][0]:
+            ind3.write('\n### ' + title[0] + '\n\n')
+    ind3.write('- [' + title + '](/Proofs/' + shortcut + '.html)\n')
 ind3.close()
+print('   - successfully written to disk!')
